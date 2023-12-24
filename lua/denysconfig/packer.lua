b9f5ca22859df661868f22fde779912146d103b5
denisvivdenko <denis.vivdenko.k@gmail.com>
2023-12-25 00:22:29 +0100

    feat: add lsp config

--- lua/denysconfig/packer.lua
+++ lua/denysconfig/packer.lua
@@ -27,6 +27,7 @@ return require('packer').startup(function(use)
 	    {'hrsh7th/cmp-nvim-lsp'},
 	    {'L3MON4D3/LuaSnip'},
 	  }
-	}
+	} 
 	use 'tpope/vim-commentary'
+	use 'neovim/nvim-lspconfig'	
 end)
